ec6b403a70c863ed07c15cd8e2199ab809d74c41
quanv23 <victorquan23@gmail.com>
2024-04-30 16:48:27 -0400

    Working on creating new card form

--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -1,24 +1,40 @@
+import { FaX } from 'react-icons/fa6';
 import React from 'react';
 
 export default function NewCardForm(props) {
 	const { toggleInputForm } = props;
 	return (
-		<div className='new-card-container' onClick={toggleInputForm}>
-			<form className='new-card'>
-				<div>
-					<input type='text' placeholder='Title' />
-					<select>
-						<option value=''>Category</option>
-						<option value='school'>School</option>
-						<option value='work'>Work</option>
-						<option value='personal'>Personal</option>
-					</select>
-					<input type='date' />
-					<input type='time' />
-					<input type='text' placeholder='location' />
+		<div className='new-card-container'>
+			<div className='new-card' onClick={(event) => event.stopPropagation()}>
+				<FaX
+					className='back'
+					size={22}
+					color='#E5383B'
+					onClick={toggleInputForm}
+				/>
+				<h2>NEW CARD</h2>
+				<h4>Title</h4>
+				<input type='text' placeholder='Title' />
+				<h4>Date</h4>
+				<input type='date' />
+				<h4>Start / End</h4>
+				<input type='time' />
+				<input type='time' />
+				<h4>Location</h4>
+				<input type='text' placeholder='location' />
+				<h4>Category</h4>
+				<select>
+					<option value=''>Category</option>
+					<option value='school'>School</option>
+					<option value='work'>Work</option>
+					<option value='personal'>Personal</option>
+				</select>
+				<div className='urgent-checkbox'>
 					<input type='checkbox' />
+					<h4>Urgent</h4>
 				</div>
-			</form>
+				<button className='add-button'>Add</button>
+			</div>
 		</div>
 	);
 }
